7d022a8646d074bd0932f81c73ba22ff34345b5c
asteroidice <ryan.s.rabello@gmail.com>
2016-03-28 16:38:48 -0700

    some changes

--- app/routes/reset-password.js
+++ app/routes/reset-password.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
+import ENV from '../config/environment';
 
 export default Ember.Route.extend({
+  actions: {
+    resetPassword() {
+      //var email = Ember.$('input[name=resetEmail]').val();
+      var ref = new Firebase(ENV.firebase);
+      //var say = function(text,color) {
+      //  Ember.$('#password-error').text(text);
+      //  Ember.$('#password-error').css("color",color);
+      //};
+      ref.resetPassword({
+        "email" : email
+      }, function(error) {
+        if (error === null) {
+          console.log("WORKED");
+          //("Password reset email sent successfully", "green");
+        } else {
+          console.log("ERR");
+          //say(error,"red");
+        }
+      });
+    }
+  }
 });
