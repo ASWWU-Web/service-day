4c39e1306a230797b298bfca405da65a024c9438
asteroidice <ryan.s.rabello@gmail.com>
2016-04-11 16:26:05 -0700

    got id-to-name working and implemented it

--- app/components/id-to-name.js
+++ app/components/id-to-name.js
@@ -1,12 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  tagName: "span",
   organization: null,
   didReceiveAttrs() {
     this._super(...arguments);
     var self = this;
     let id = this.get('studentID');
-    return this.get('store').findRecord('organization', {'id': id}).then((data) => {
+    if(!(id)|| id === null || id === ""){
+      return null;
+    }
+    return this.get('store').findRecord('organization', id).then((data) => {
       self.set('organization', data);
     });
   }
