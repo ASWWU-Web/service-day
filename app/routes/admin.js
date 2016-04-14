d820ad367c44b97475acee9b40c599b033786f19
asteroidice <ryan.s.rabello@gmail.com>
2016-04-13 17:35:41 -0700

    csv working properly now

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -28,6 +28,7 @@ export default Ember.Route.extend({
     },
     csv(record) {
       var ref;
+      //declare firebase object
       try{
         ref = new Firebase(ENV.firebase + record);
       }
@@ -35,40 +36,82 @@ export default Ember.Route.extend({
         alert("There was an Error" + er);
         return;
       }
-      var recordData;
-      ref.once("value", (data) => {
-        recordData = data.val();
-      });
-      var data = ['<records>'];
-      var regex = /[\n"/<>'&]/g;
-      Ember.$.each(recordData, (key,value) => {
-        var subData = "<record>";
-        Ember.$.each(value,(k,v) => {
-          try{
-            subData += "<" + k + ">" + v.replace(regex,"") + "</" + k + ">\n";
-          }
-          catch(er){
-            subData += "<" + k + ">" + v + "</" + k + ">\n";
-          }
-        });
-        subData += "</record>";
-        data.push(subData);
-      });
-      data.push('</records>');
-      var csvContent = data.join('\n');
-      try {
-        var a = document.createElement('a');
-        a.href = 'data:attachment/xml,' +  encodeURIComponent(csvContent);
-        a.target = '_blank';
-        a.download = record + '.xml';
+      var projectID2name = function(id) {
+        var responce;
+        try {
+          ref.root().child("organizations/" + id).once('value', (data) => {
+            try {
+              responce = data.val().name;
+            } catch(er) {
+              responce = "PROJECT REMOVED!";
+            }
+          });
+        } catch(er) {
+          responce = "PROJECT REMOVED!";
+        }
 
-        document.body.appendChild(a);
-        a.click();
-        a.remove();
-      }
-      catch(er){
-        alert("There was an error. Try using Firefox or Chome.\n" + er);
-      }
+        return responce;
+      };
+
+      //get records from firebase
+      ref.once("value", function(recordData) {
+        recordData = recordData.val();
+        //this the array that gets joined together in the end
+        var data = [];
+        var regex = /[\n"/<>'&]/g;
+        //this is what the csv will be ordered by. (name is first)
+        var keys = ["name"];
+
+        //go through all the record objects
+        Ember.$.each(recordData, function(key,value) {
+          //put any data whose key is already known into subdata[]
+          var subData = [];
+          for (var i = 0; i < keys.length; i++){
+            var k = keys[i];
+            if (k in value){
+              //convert projectID to project name if necessary.
+              if(k === "projectID" && value[k] != "" && value[k] != null){
+                subData.push('"' + projectID2name(value[k]).replace(regex,"") + '"');
+              } else {
+                subData.push('"' + String(value[k]).replace(regex,"") + '"');
+              }
+              //remove the value from the json so that it won't be used again.
+              delete value[k];
+            } else {
+              subData.push('" "');
+            }
+          }
+          //add the remaining elements and make sure not to add any duplicates.
+          Ember.$.each(value, function(k,v) {
+            //add k to keys[]
+            keys.push(k);
+            // and add v to subData[] (also check if it is a projectID)
+            if(k === "projectID" && v != "" && v != null){
+              subData.push('"' + projectID2name(v).replace(regex,"") + '"');
+            } else {
+              subData.push('"' + String(v).replace(regex,"") + '"');
+            }
+          });
+          data.push(subData.join(','));
+        });
+
+        //download file to client using HTML5
+        var csvContent = data.join('\n');
+        try {
+          var a = document.createElement('a');
+          a.href = 'data:attachment/csv,' +  encodeURIComponent(csvContent);
+          a.target = '_blank';
+          a.download = record + '.csv';
+
+          document.body.appendChild(a);
+          a.click();
+          a.remove();
+        }
+        catch(er){
+          alert("There was an error. Try using Firefox or Chome.\n" + er);
+        }
+      });
+
 
     }
   }
